d7960acd6c0013ae2797b821f66ae102941916ba
MhaamadAli <m.alialaawar@gmail.com>
2024-03-15 12:41:24 +0200

    added load tasks from backend

--- backend/edit_todo.php
+++ backend/edit_todo.php
@@ -3,27 +3,21 @@ include './connection.php';
 
 $user_id = $_POST['user_id'];
 $todo_id = $_POST['todo_id'];
-$isdone = isset($_POST['isCompleted']) ? $_POST['isCompleted'] : null;
-$description = isset($_POST['description']) ? $_POST['description'] : null;
+$isdone = isset($_POST['isCompleted'])? $_POST['isCompleted'] : null;
+$description = isset($_POST['description'])? $_POST['description'] : null;
 
-// Construct the query based on the provided parameters
-if ($description !== null && $isdone !== null) {
-    $query = $mysqli->prepare('UPDATE todos SET description=?, isCompleted=? WHERE todo_id=? AND user_id=?');
-    $query->bind_param('siii', $description, $isdone, $todo_id, $user_id);
-} elseif ($description !== null) {
-    $query = $mysqli->prepare('UPDATE todos SET description=? WHERE todo_id=? AND user_id=?');
+
+if($description) {
+    $query = $mysqli ->prepare('UPDATE todos SET description= ? WHERE todo_id= ? AND user_id= ? ');
     $query->bind_param('sii', $description, $todo_id, $user_id);
-} elseif ($isdone !== null) {
-    $query = $mysqli->prepare('UPDATE todos SET isCompleted=? WHERE todo_id=? AND user_id=?');
-    $query->bind_param('iii', $isdone, $todo_id, $user_id);
-} else {
-    // If no valid parameters provided, return an error response
-    $response['message'] = "No valid parameters provided for update";
-    echo json_encode($response);
-    exit; // Terminate further execution
+}elseif($isdone) {
+    $query = $mysqli ->prepare('UPDATE todos SET isCompleted= ? WHERE todo_id= ? AND user_id= ? ');
+    $query->bind_param('s', $isdone, $todo_id, $user_id);
+}elseif ($isdone && $description) {
+    $query = $mysqli ->prepare('UPDATE todos SET description= ?, isdone= ? WHERE todo_id= ? AND user_id= ? ');
+    $query->bind_param('ss', $description, $isdone, $todo_id, $user_id);
 }
-
-if ($query->execute()) {
+if($query->execute()){
     $response['message'] = "edited successfully";
 } else {
     $response['message'] = "error not updated";
@@ -31,5 +25,4 @@ if ($query->execute()) {
 
 echo json_encode($response);
 
-$mysqli->close();
-?>
+$mysqli ->close();
